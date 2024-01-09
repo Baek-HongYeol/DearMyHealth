1e0fef9e5252638a3702f835eba59329bb803002
Baek-HongYeol <bac25450@gmail.com>
2024-01-09 20:06:26 +0900

    FIX: dependancy 라이브러리 버전 변경
    stable 버전으로 변경함.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,6 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -50,15 +44,16 @@ android {
 }
 
 dependencies {
-    implementation("com.google.android.material:material:1.11.0")
+    val lifecycle_version = "2.5.1"
+    val room_version = "2.5.0"
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
     implementation ("androidx.health:health-connect-client:1.0.0-alpha03")
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation ("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
