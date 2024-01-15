f348021910132a7ae7dcf51f7f584071074be30c
Baek-HongYeol <bac25450@gmail.com>
2024-01-15 17:36:15 +0900

    FEAT: 기본 레이아웃 구현
    Toolbar, NavigationDrawer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,14 +46,23 @@ android {
 dependencies {
     val lifecycle_version = "2.5.1"
     val room_version = "2.5.0"
+    val nav_version = "2.5.3"
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
+
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
     implementation ("androidx.health:health-connect-client:1.0.0-alpha03")
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation ("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/dearmyhealth/MainActivity.kt
+++ app/src/main/java/com/dearmyhealth/MainActivity.kt
@@ -1,12 +1,60 @@
 package com.dearmyhealth
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dearmyhealth.R
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout : DrawerLayout
+    private val TAG = this.javaClass.simpleName
+    private var backPressedTime: Long = 0
+    private val onBackpressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            onBackPressedKey()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        initView()
+        this.onBackPressedDispatcher.addCallback(onBackpressedCallback)
+    }
+    fun initView() {
+        var toolbar : Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    fun onBackPressedKey() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+            drawerLayout.closeDrawer(GravityCompat.END)
+        }
+        else if(System.currentTimeMillis() > backPressedTime+2000) {
+            backPressedTime = System.currentTimeMillis()
+            Toast.makeText(this, "'뒤로' 버튼을 한 번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show()
+        }
+        else if(System.currentTimeMillis() <= backPressedTime + 2000) {
+            finish()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_home_toolbar, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.home_drawer_menu -> {
+                drawerLayout.openDrawer(GravityCompat.END)}
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
