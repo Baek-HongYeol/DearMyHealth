cbf7daf42bc8e2c30e9b33ff05ceb401f057e368
Baek-HongYeol <bac25450@gmail.com>
2024-03-28 11:58:28 +0900

    Add: 개인정보 처리방침 보여줄 페이지 추가
    TextView only

---
+++ app/src/main/java/com/dearmyhealth/PrivacyPolicyActivity.kt
@@ -0,0 +1,12 @@
+package com.dearmyhealth
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PrivacyPolicyActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_privacy_policy)
+    }
+}
