185b6ca79d7e39c8453340c3adcc718b15e60df0
Baek-HongYeol <bac25450@gmail.com>
2024-03-28 11:56:45 +0900

    Feat: HealthConnectManager 추가. 설치 여부 확인
    1. HealthConnectManager 추가.
        HealthConnectClient를 application 단위에서 생성하도록 함.
    
    2. DeptFragment에서 HealthConnect 설치 여부 확인.
        미설치 시 스토어로 이동 팝업 노출,
            설치 취소 시, 뒤로가기
        미지원 시 뒤로가기.

--- app/src/main/java/com/dearmyhealth/MyApplication.kt
+++ app/src/main/java/com/dearmyhealth/MyApplication.kt
@@ -1,15 +1,21 @@
 package com.dearmyhealth
 
 import android.app.Application
+import com.dearmyhealth.data.db.AppDatabase
+import com.dearmyhealth.modules.healthconnect.HealthConnectManager
 
 class MyApplication: Application() {
 
-    init {
-        INSTANCE = this
+    val healthConnectManager by lazy {
+        HealthConnectManager(this)
     }
 
+    val database by lazy {
+        AppDatabase.getDatabase(this)
+    }
     companion object {
         private lateinit var INSTANCE: MyApplication
         fun ApplicationContext() = INSTANCE.applicationContext
     }
+
 }
--- app/src/main/java/com/dearmyhealth/modules/bpm/DeptFragment.kt
+++ app/src/main/java/com/dearmyhealth/modules/bpm/DeptFragment.kt
@@ -1,29 +1,79 @@
 package com.dearmyhealth.modules.bpm
 
+import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.dearmyhealth.R
+import com.dearmyhealth.databinding.FragmentDeptBinding
+import com.dearmyhealth.modules.healthconnect.HealthConnectAvailability
+import com.dearmyhealth.modules.healthconnect.HealthConnectManager
 
 class DeptFragment : Fragment() {
+    lateinit var binding: FragmentDeptBinding
+    lateinit var healthConnectManager: HealthConnectManager
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_dept, container, false)
-        val bpmButton = view.findViewById<ImageButton>(R.id.bpmButton)
+    ): View {
+        binding = FragmentDeptBinding.inflate(inflater)
+        val view = binding.root
+        val bpmButton = binding.bpmButton
         bpmButton.setOnClickListener { openFragment(BpmFragment()) }
-        val tempButton = view.findViewById<ImageButton>(R.id.tempButton)
+        val tempButton = binding.tempButton
         tempButton.setOnClickListener { openFragment(TempFragment()) }
-        val weightButton = view.findViewById<ImageButton>(R.id.weightButton)
+        val weightButton = binding.weightButton
         weightButton.setOnClickListener { openFragment(WeightFragment()) }
-        val stepButton = view.findViewById<ImageButton>(R.id.Buttonstep)
+        val stepButton = binding.Buttonstep
         stepButton.setOnClickListener { openFragment(StepFragment()) }
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        healthConnectManager = HealthConnectManager(requireContext().applicationContext)
+        healthConnectManager.checkAvailability()
+
+        checkAvailability()
+    }
+
+    private fun checkAvailability() {
+        when (healthConnectManager.availability.value) {
+            HealthConnectAvailability.NOT_INSTALLED ->
+                AlertDialog.Builder(requireContext())
+                    .setMessage("헬스 커넥트가 설치되어 있지 않습니다.\n설치하기 위해 스토어로 가시겠습니까?")
+                    .setNegativeButton(R.string.cancel) { _, _, -> findNavController().popBackStack() }
+                    .setPositiveButton("이동") { _, _ ->
+                        // Optionally redirect to package installer to find a provider, for example:
+                        requireContext().startActivity(
+                            Intent(Intent.ACTION_VIEW).apply {
+                                setPackage("com.android.vending")
+                                data = Uri.parse("market://details")
+                                    .buildUpon()
+                                    .appendQueryParameter("id", getString(R.string.health_connect_package))
+                                    .appendQueryParameter("url", "healthconnect://onboarding")
+                                    .build()
+                                putExtra("overlay", true)
+                                putExtra("callerId", requireContext().packageName)
+                            }
+                        )
+                        return@setPositiveButton
+                    }.show()
+            HealthConnectAvailability.NOT_SUPPORTED -> {
+                AlertDialog.Builder(requireContext())
+                    .setMessage("헬스 커넥트가 지원되지 않는 기기입니다.")
+                    .show()
+                findNavController().popBackStack()
+            }
+            else -> return
+        }
     }
 
     private fun openFragment(fragment: Fragment) {
